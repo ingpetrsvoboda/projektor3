15e808f96a0fedbc87a30946840dddbfcb6eaff2
pes2704 <pes2704@nb-svoboda.intranet.grafia.cz>
2012-02-17 17:38:44 +0100

    20120217_1738

--- COM_test.php
+++ COM_test.php
@@ -1,5 +1,5 @@
 <?php
-
+//1
   $shell= &new COM('WScript.Shell');
     var_dump($shell->regRead('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine\Distinguished-Name'));
     var_dump($shell->regRead('HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName\ComputerName'));
